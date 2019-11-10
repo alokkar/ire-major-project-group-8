21519d066bd5f6043699b4eaff0a7a38303984c2
Tanuj Garg <tanujgarg@pop-os.localdomain>
2019-11-10 14:57:14 +0530

    updated results after machine translation data augmentation

--- SVM/hs_svm.py
+++ SVM/hs_svm.py
@@ -1,9 +1,10 @@
 import sys,os
 import pandas as pd
 import numpy as np
+import re
+import matplotlib.pyplot as plt
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
-import re
 
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
@@ -64,7 +65,8 @@ def preprocess(tweet):
 	return ' '.join(stemmed_text_token)
 
 def classifier(data,labels):
-	X_train,X_test,y_train,y_test = data[:3400],data[-383:],labels[:3400],labels[-383:]
+	X_train,X_test,y_train,y_test = train_test_split(data, labels, test_size=0.2, random_state=1)
+	# X_train,X_test,y_train,y_test = data[:3400],data[-383:],labels[:3400],labels[-383:]
 	text_clf = Pipeline([('vect', CountVectorizer(ngram_range=(1, 2))),
 					 ('tfidf', TfidfTransformer(use_idf=False,norm='l2')),
 					 ('clf', SGDClassifier(loss='hinge', penalty='l2',
@@ -97,39 +99,105 @@ def classifier(data,labels):
 	text_clf.fit(X_train,y_train)
 	prediction = text_clf.predict(X_test)
 	accuracy_score = text_clf.score(X_test,y_test)
-	print("Accuracy : ",accuracy_score) #accuracy score
-	print("f1_score : ",f1_score(y_test, prediction, average="macro")) #f1_score
-	print("Precision : ",precision_score(y_test, prediction, average="macro")) #precision_score
-	print("Recall : ",recall_score(y_test, prediction,average="macro")) #recall_score
-	print("---------------Example---------------")
-	print("Statement : i will kill all the immigrants")
-	print(text_clf.predict(["i will kill all the immigrants"])) 
-	print("Statement : i will kiss all the immigrants")
-	print(text_clf.predict(["i will kiss all the immigrants"])) 
+	# print("Accuracy : ",accuracy_score) #accuracy score
+	# print("f1_score : ",f1_score(y_test, prediction, average="macro")) #f1_score
+	# print("Precision : ",precision_score(y_test, prediction, average="macro")) #precision_score
+	# print("Recall : ",recall_score(y_test, prediction,average="macro")) #recall_score
+	# print("---------------Example---------------")
+	# print("Statement : i will kill all the immigrants")
+	# print(text_clf.predict(["i will kill all the immigrants"])) 
+	# print("Statement : i will kiss all the immigrants")
+	# print(text_clf.predict(["i will kiss all the immigrants"])) 
+	return text_clf
 
-
-def main():
-
-	filename = './train_en.tsv'
+def get_vars(filename):
 	raw_data,labels_TR,labels_AG,labels_HS = load_data(filename)
 
 	data = [preprocess(tweet) for tweet in raw_data]
 	X = []
 	y_AG = []
 	y_TR = []
-	count = 0
 	for index,word in enumerate(labels_HS):
 		if word:
 			X.append(data[index])
 			y_AG.append(labels_AG[index]) 
 			y_TR.append(labels_TR[index])
-			count += 1
+	return X, y_AG, y_TR
 
-	# print(count)
+def test(filename, clf_AG, clf_TR):
+	X, y_AG, y_TR = get_vars(filename)
 
-	classifier(X,y_TR)
+	prediction_AG = clf_AG.predict(X)
+	prediction_TR = clf_TR.predict(X)
+	f1_ag = f1_score(y_AG, prediction_AG, average="macro")
+	f1_tr = f1_score(y_TR, prediction_TR, average="macro")
+	print("-----------Aggressive Task------------")
+	print("Accuracy : ",clf_AG.score(X, y_AG)) #accuracy score
+	print("f1_score : ",f1_ag) #f1_score
+	print("Precision : ",precision_score(y_AG, prediction_AG, average="macro")) #precision_score
+	print("Recall : ",recall_score(y_AG, prediction_AG,average="macro")) #recall_score
+	print("------------Target Task-------------")
+	print("Accuracy : ",clf_TR.score(X, y_TR)) #accuracy score
+	print("f1_score : ",f1_tr) #f1_score
+	print("Precision : ",precision_score(y_TR, prediction_TR, average="macro")) #precision_score
+	print("Recall : ",recall_score(y_TR, prediction_TR,average="macro")) #recall_score
+	return f1_ag, f1_tr
 
-	# print(labels_AG[:100])
+def make_plot(results_eda, results_tr, plot_title, yrange):
+    x = range(1,5)
+    plt.plot(x, results_eda)
+    plt.plot(x, results_tr)
+    plt.xticks(x, x)
+    plt.xlabel("Size of Dataset")
+    plt.ylabel("F1 score")
+    plt.ylim(yrange[0], yrange[1])
+    plt.title(plot_title)
+    plt.legend(['EDA', 'Machine Translation'])
+    plt.show()
+
+def main():
+
+	filename = '../Dataset/train_en.tsv'
+	test_file = '../Dataset/dev_en.tsv'
+	X, y_AG, y_TR = get_vars(filename)
+	clf_AG = classifier(X, y_AG)
+	clf_TR = classifier(X, y_TR)
+
+	print("=====================================================")
+	print("Original Dataset")
+	f1_ag, f1_tr = test(test_file, clf_AG, clf_TR)
+	transl_ag = [f1_ag]
+	transl_tr = [f1_tr]
+	eda_ag = [f1_ag]
+	eda_tr = [f1_tr]
+
+	print("=====================================================")
+	print("Data Augmentation using EDA technique")
+	for degree in range(3):
+		print("-------------------------------------------------")
+		print("Dataset of size * {}".format(degree+2))
+		X, y_AG, y_TR = get_vars('../Dataset/eda_aug/data_degree_{}.tsv'.format(degree+2))
+		clf_AG = classifier(X, y_AG)
+		clf_TR = classifier(X, y_TR)
+		f1_ag, f1_tr = test(test_file, clf_AG, clf_TR)
+		eda_ag.append(f1_ag)
+		eda_tr.append(f1_tr)
+
+	print("=====================================================")
+	print("Data Augmentation using Machine Translation")
+	for degree in range(3):
+		print("-------------------------------------------------")
+		print("Dataset of size * {}".format(degree+2))
+		X, y_AG, y_TR = get_vars('../Dataset/transl_aug/data_degree_{}.tsv'.format(degree+2))
+		clf_AG = classifier(X, y_AG)
+		clf_TR = classifier(X, y_TR)
+		f1_ag, f1_tr = test(test_file, clf_AG, clf_TR)
+		transl_ag.append(f1_ag)
+		transl_tr.append(f1_tr)
+
+	make_plot(eda_ag, transl_ag, "Aggression results after data augmentation", [0.55, 0.6])
+	make_plot(eda_tr, transl_tr, "Target results after data augmentation", [0.88, 0.91])
+
 
 if __name__=='__main__':
 	main()
